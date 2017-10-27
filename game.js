c67adf4c6d62fce02502431df25203545f30c1fe
Jeff Smith <jeffsmith89423@gmail.com>
2017-10-27 16:26:35 -0600

    progress Save

--- game.js
+++ game.js
@@ -1,30 +1,62 @@
-var health=100;
+ 
+function Player() {
+    this.health = health;
+    this.name= name;
+    this.hits = hits;
+    
+}
+
+function Items(){
+    this.name = name;
+    this.modifier = modifier;
+    this.description = description;
+}
+
+var items = {
+    shield:new Item("Shield",0.3,"This is an awesome shield!"),
+    balloon:new Item("Balloon",0.5,"Don't be fooled by this globe!"),
+    shoe:new Item("Shoe",0.4,"Big, red and ridiculus")
+} 
+
+
+var player1 = {
+    health: 100,
+    name: Bozo,
+    hits: 0,
+    items:[giveShield.push() 
+    ]
+}
+var player2 = {
+    health: 100,
+    name: Krusty,
+    hits: 0,
+    items:[ 
+    ]
+}
+
 
 
 function slap(){
  health -= 1;
-  return health;
-  document.getElementById("health").innerHTML= "test"
-  
+ hits += 1;
+  update()
 }
 function punch(){
-    health -= 10
-    return health;
+    health -= 5;
+    hits += 1;
+    update()
 }
-function suplex(){
-    health -= 20
-    return health;
+function kick(){
+    health -= 10;
+    hits += 1;
+    update()
 }
 function update(){
-    
+    document.getElementById("target.health").innerHTML= "Health = " + target.health + "%";
+    document.getElementById("target.hits").innerHTML= "Hits = " + target.hits
+    document.getElementById("target.name").innerHTML= "Name = " + target.name
 }
 
-// function draw(endGame, playerChoice, compChoice){
-//     document.getElementById("compChoice").innerHTML = "The computer chose " + compChoice + ".";
-//     document.getElementById("playerChoice").innerText
-//         = "and you chose " + playerChoice + ".";
-//     document.getElementById("endGame").innerText
-//         = endGame + ".";
 
 
 
@@ -56,23 +88,9 @@ function update(){
 
 
 
-//     var reduceHealth = health - 1;
-//     if (slap === 1){
-//      endGame = "health = "
-//     }
-//     return health
-// }
-    
 
 
-//  function draw(reduceHealth){
-//     document.getElementById("slap('reducedHealth')").innerHTML = endGame + "test"
-    
 
-//  }
-
-// console.log(slap(1))
-// console.log(draw)
 
 
 
